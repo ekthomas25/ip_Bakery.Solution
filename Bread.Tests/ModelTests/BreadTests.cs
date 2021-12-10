3f961a5dcb7a192edae2571bb015ac2674279ef4
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-10 10:22:55 -0800

    Create Test 2: Fail - compiler error, no constructor or property

--- Bread.Tests/ModelTests/BreadTests.cs
+++ Bread.Tests/ModelTests/BreadTests.cs
@@ -12,5 +12,16 @@ namespace Bakery.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+    [TestMethod]
+    public void GetBread_ReturnBreadType_String()
+    {
+      //Arrange - Gather, declate, create: necessary components for test
+      string breadType = "Baguette";
+      Bread newBread = new Bread(breadType);
+      // Act - Invoke functionality: ie call method, retrieve property, etc
+      string result = newBread.breadType;
+      //Assert - Confirm functionality works: compare actual output to expected output
+      Assert.AreEqual(breadType, result);
+    }
   }
 }
