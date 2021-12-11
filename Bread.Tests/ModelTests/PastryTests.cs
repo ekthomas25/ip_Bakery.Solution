2f30548160c2112c5daae5d3e539e4a6a9f5df78
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-10 17:30:05 -0800

    Create Test 8: Automatically Passing

---
+++ Bread.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreateInstanceOfPastry_Pastry()
+    {
+      Pastry nwePastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), nwePastry.GetType());
+    }
+  }
+}
---
+++ Bread/Models/Pastry.cs
@@ -0,0 +1,21 @@
+
+namespace Bakery.Models
+{
+ public class Pastry
+  {
+    
+  }
+}
+
+/*
+  Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.
+
+Essentially: Buy 3 get one half off
+
+PastryQty = 1
+PastryPrice = 2
+
+PastryQty = 3
+PastryPrice = 5
+
+*/
